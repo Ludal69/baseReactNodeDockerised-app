e9016e57fd34b3618387787878559b86c15da355
DESKTOP-G4I7JNL\Ludo <lulu.713lg@gmail.com>
2024-06-23 16:03:38 +0200

    - hot reload frontend avec watch docker - pour le debugger côté front, on utilise devtools sur chrome. Sur vs code on a juste le debugger du back qui est lancé.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,11 +10,19 @@ function App() {
       .then((data) => setBlogs(data));
   }, []);
 
+  const showAlert = () => {
+    alert("bonjour");
+    fetch("http://localhost:4000/")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>all blogs</h1>
         {blogs && blogs.map((blog) => <div key={blog.id}>{blog.title}</div>)}
+        <button onClick={showAlert}>Show Alert</button>
       </header>
     </div>
   );
